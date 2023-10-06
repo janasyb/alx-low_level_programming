0ad86547cde6d51e476d154132a59e5e421be34f
janasyb <jayettey31@gmail.com>
2023-10-05 19:20:24 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <std.io>
 /**
   * main - this is a main function
-  * Return: 0 if success
+  * Return: 0
   */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
