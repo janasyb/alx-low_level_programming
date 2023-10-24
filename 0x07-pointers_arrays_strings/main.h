d67be7c05f6fe6cc92bbceeb6f2c7fa4670acc14
janasyb <jayettey31@gmail.com>
2023-10-23 21:46:08 -0700

    5-strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,11 +3,12 @@
 
 #include	<stdio.h>
 #include	<stdlib.h>
+#include	<string.h>
 int	_putchar(char	c);
 char	*_memset(char	*s,	char	b,	unsigned	int	n);
 char	*_memcpy(char	 *dest,	char	*src,	unsigned	int	n);
 char	*_strchr(char	*s,	char	c);
-unsigned	int	_strspn(char	*s,	char	*accept);
+extern size_t strspn(const char	*_s,	const	char	*_accept);
 char	*_strpbrk(char	*s,	char	*accept);
 char	const(haystack);
 void	print_chessboard(char	(*a)[8]);
